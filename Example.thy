611bb9ca6a0ebb943fe4370e6eab38481b6f22a8
xxan15 <xxan15@vt.edu>
2015-11-30 14:50:56 -0500

    Working on the increment by 2 example

--- Example.thy
+++ Example.thy
@@ -2,16 +2,36 @@ theory Example
 imports Proof_Par
 begin
 
-abbreviation "wsum ==
-  Parallel [((Some (ABasic (\<lambda>s. s ''x'' = (0::int)) (\<lambda>s. s(''x'' := aval (Plus (V ''x'') (N 1)) s)))),(\<lambda>s. s ''x'' = aval (Plus (V ''x'') (N 1)) s)),
-    ((Some (ABasic (\<lambda>s. s ''x'' = (0::int)) (\<lambda>s. s(''x'' := aval (Plus (V ''x'') (N 1)) s)))),(\<lambda>s. s ''x'' = aval (Plus (V ''x'') (N 1)) s))]"
+text {* Goal: \<turnstile>\<^sub>P {x = 0} x := x+1 || x:= x+1 {x = 2} *}
+
+text {* 
+Annotated program: 
+  {x = 0, d1 = false, ...} (x := x+1, d1 := true) {x = x+1} 
+  || 
+  {x = 0, d2 = false, ...} (x:= x+1, d2 := true) {x = x+1} 
+
+Goal: \<turnstile>\<^sub>P {x = 0, d1 = false, d2 = false} x := x+1 || x:= x+1 {x = 2}
+*}
+
+abbreviation "pre1 \<equiv> \<lambda> s . True"
+abbreviation "pre2 \<equiv> \<lambda> s . True"
+abbreviation "post1 \<equiv> \<lambda> s . True"
+abbreviation "post2 \<equiv> \<lambda> s . True"
+
+abbreviation "plus11 \<equiv> \<lambda>s. s(''x'' := aval (Plus (V ''x'') (N 1)) s, ''d1'' := 1)"
+
+abbreviation "plus12 \<equiv> \<lambda>s. s(''x'' := aval (Plus (V ''x'') (N 1)) s, ''d2'' := 1)"
+
+abbreviation "plus2 ==
+  Parallel [((Some (ABasic pre1 (plus11))), post1),
+    ((Some (ABasic pre2 (plus12))), post1)]"
 
 subsubsection{* Proof by Hoare Logic *}
 
 text{* Note that we deal with sequences of commands from right to left,
 pulling back the postcondition towards the precondition. *}
 
-lemma "\<turnstile>\<^sub>P {\<lambda>s. s x = 0} wsum {\<lambda>s. s x = 2}"
+lemma "\<turnstile>\<^sub>P {\<lambda>s. s x = 0} plus2 {\<lambda>s. s x = 2}"
 apply(rule PParallelE)
 
 end
